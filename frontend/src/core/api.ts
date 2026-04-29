41600722c750a35afb8fd72e090b1d6b345d258b
Lucas-c13 <lucas.a13c@gmail.com>
2026-04-28 21:27:17 -0300

    front

--- frontend/src/core/api.ts
+++ frontend/src/core/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://regulacao-sus-saas-production.up.railway.app', // Substitui pela tua URL real
 });
 
 // Interceptor de REQUISIÇÃO (que você já tem)
@@ -15,7 +15,7 @@ api.interceptors.request.use((config) => {
 
 // 👇 Interceptor de RESPOSTA Re-arquitetado para Router
 api.interceptors.response.use(
-  (response) => response, 
+  (response) => response,
   (error) => {
     // Se o erro for 401 (Não Autorizado) -> Dispara o evento global
     // O InternalAuthObserver no App.tsx agarra nisto e redireciona de forma suave!
